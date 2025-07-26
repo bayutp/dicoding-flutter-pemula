ea5770197f6bc86e0d66fc502a2c41a8a5e7d22b
bayu <bayuteguhpamuji@gmail.com>
2025-07-26 10:08:30 +0700

    Restructured project files, added images and font assets, and improved the UI using ScrollView and ListView.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,68 +17,6 @@ class MyApp extends StatelessWidget {
       ),
       home: const DetailScreen(),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              margin: const EdgeInsets.all(16),
-              child: Text(
-                "Farm House Lembang",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8),
-                      Text('Open Everyday'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8),
-                      Text('09.00 - 20.00'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8),
-                      Text('Rp 25.000'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: Text(
-                "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
