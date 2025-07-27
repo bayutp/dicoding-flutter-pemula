c29efe990d63ef0f5346a211b698a8c8d499c811
bayu <bayuteguhpamuji@gmail.com>
2025-07-27 11:36:15 +0700

    Add dummy data and display it in ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_apps/detail_screen.dart';
+import 'package:flutter_apps/model/tourism_place.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,39 +15,48 @@ class HomeScreen extends StatelessWidget {
         ),
         backgroundColor: Colors.amber,
       ),
-      body: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DetailScreen()),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final TourismPlace tourismPlace = tourismPlaceList[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => DetailScreen()),
+              );
+            },
+            child: Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image.asset(tourismPlace.imageAsset),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            tourismPlace.name,
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                          SizedBox(height: 10),
+                          Text(tourismPlace.location),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           );
         },
-        child: Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(flex: 1, child: Image.asset('images/farm-house.jpg')),
-              Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "Farm House Lembang",
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                      SizedBox(height: 10),
-                      Text("Lembang"),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        itemCount: tourismPlaceList.length,
       ),
     );
   }
